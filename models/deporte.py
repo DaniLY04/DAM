2b3384286b78ecb6ad0e6620f21a4542e0db7558
root <root@ED09012096P308.educa.jcyl.es>
2025-01-27 20:29:09 +0100

    Actualizacion modelos

--- models/deporte.py
+++ models/deporte.py
@@ -1,8 +1,8 @@
 from odoo import models, fields, api
 
 class deporte(models.Model):
-    _name = 'trabajo_final.partido'
-    _description = 'trabajo_final.partido'
+    _name = 'trabajo_final.deporte'
+    _description = 'trabajo_final.deporte'
 
     id_deporte = fields.Integer(string = "id", required="true")
     nombre = fields.Char(string="Nombre")
