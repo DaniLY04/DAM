67623045ef45a695b0b3ddd2c5fd3b69d053968d
DaniLY04 <root@ED09012096P302.educa.jcyl.es>
2025-01-29 20:10:40 +0100

    Player Sequencia bien

--- models/player.py
+++ models/player.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Player(models.Model):
@@ -6,7 +6,7 @@ class Player(models.Model):
     _description = 'Jugadores'
 
     name = fields.Char(string="Nombre", required = True)
-    id_player = fields.Char(string="ID_Jugador", default=lambda self: self.env['ir.sequence'].next_by_code('increment_idplayer_sequence'), readonly=True)
+    id_player = fields.Char(string="ID_Jugador", readonly=True)
     last_name = fields.Char(string ="Apellidos", requiered = True)
 
     gender = fields.Selection([
@@ -15,3 +15,9 @@ class Player(models.Model):
     ], string="Genero", required = True)
 
     image = fields.Binary(string="Foto")
+
+    @api.model
+    def create(self, vals):
+        if 'id_player' not in vals or not vals['id_player']:
+            vals['id_player'] = self.env['ir.sequence'].next_by_code('increment_idplayer_sequence')
+        return super(Player, self).create(vals)
